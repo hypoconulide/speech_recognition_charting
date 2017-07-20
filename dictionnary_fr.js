6b254ca786bc4560dc5a1e91495483096efb8f2b
philippe-bachour <pbachour@gmail.com>
2017-07-20 22:06:19 +0200

    Furcation box css fix for highres screen and printing. Statistic charts added.

--- dictionnary_fr.js
+++ dictionnary_fr.js
@@ -10,15 +10,21 @@ var __dictionnary = {
     date:'Date',
     mobility:'Mobilité',
     implant:'Implant',
+    implants:'Implants',
     furcation:'Furcation',
     bleed_on_probing:'Saignement au sondage',
     plaque:'Plaque',
     gingival_depth:'Niveau gingival',
+    average_gingival_depth:'Niveau gingival moyen',
     probing_depth:'Progondeur de sondage',
+    average_probing_depth:'Progondeur de sondage moyenne',
     start:'Démarrer',
     stop:'Arrêter',
     refresh:'Rafraîchir',
     periodontal_chart:'Charting Parodontal',
+
+    //statistics
+    missing_teeth:'Dents absentes',
 
     //voice control
     missing:'absente',
--- ng_controller.js
+++ ng_controller.js
@@ -12,6 +12,17 @@ app.controller('ctl', function($scope){
     $scope.TeethList = TeethMajor;
     $scope.Dictionnary = __dictionnary;
 
+    $scope.MissingTeethRatio = 0;
+
+
+    $scope.SVGPlaque = function(sum){
+        var Circumference = 2 * Radius * Math.PI;
+
+        var maxCount = 192;
+
+        var offset = -(Circumference / maxCount) * sum;
+        document.getElementById('crt_plaque_c').setAttribute('stroke-dashoffset', offset);
+    };
     $scope.sumPlaque = function(){
         var sum = 0;
         for (var i = 0 ; i < $scope.UpperTeeth.length ; i++)
@@ -29,7 +40,17 @@ app.controller('ctl', function($scope){
             sum += $scope.LowerTeeth[i].m_HasPlaqueL.b;
             sum += $scope.LowerTeeth[i].m_HasPlaqueL.c;
         }
+        $scope.SVGPlaque(sum);
         return Math.round(sum / 1.92 * 100) / 100;
+    };
+    
+    $scope.SVGBleed = function(sum){
+        var Circumference = 2 * Radius * Math.PI;
+
+        var maxCount = 192;
+
+        var offset = -(Circumference / maxCount) * sum;
+        document.getElementById('crt_bleed_c').setAttribute('stroke-dashoffset', offset);
     };
     $scope.sumBleed = function(){
         var sum = 0;
@@ -48,7 +69,58 @@ app.controller('ctl', function($scope){
             sum += $scope.LowerTeeth[i].m_BleedOnProbingL.b;
             sum += $scope.LowerTeeth[i].m_BleedOnProbingL.c;
         }
+        $scope.SVGBleed(sum);
         return Math.round(sum / 1.92 * 100) / 100;
+    };
+
+    $scope.averageGingivalDepth = function(){
+        var sum = 0;
+        for (var i = 0 ; i < $scope.UpperTeeth.length ; i++)
+        {
+            sum += $scope.UpperTeeth[i].m_GingivalMargin.a;
+            sum += $scope.UpperTeeth[i].m_GingivalMargin.b;
+            sum += $scope.UpperTeeth[i].m_GingivalMargin.c;
+            sum += $scope.UpperTeeth[i].m_GingivalMarginL.a;
+            sum += $scope.UpperTeeth[i].m_GingivalMarginL.b;
+            sum += $scope.UpperTeeth[i].m_GingivalMarginL.c;
+            sum += $scope.LowerTeeth[i].m_GingivalMargin.a;
+            sum += $scope.LowerTeeth[i].m_GingivalMargin.b;
+            sum += $scope.LowerTeeth[i].m_GingivalMargin.c;
+            sum += $scope.LowerTeeth[i].m_GingivalMarginL.a;
+            sum += $scope.LowerTeeth[i].m_GingivalMarginL.b;
+            sum += $scope.LowerTeeth[i].m_GingivalMarginL.c;
+        }
+        var average = Math.round(sum / 192 * 100) / 100;
+        return average;
+    };
+    $scope.averageProbingDepth = function(){
+        var sum = 0;
+        for (var i = 0 ; i < $scope.UpperTeeth.length ; i++)
+        {
+            sum += $scope.UpperTeeth[i].m_ProbingDepth.a;
+            sum += $scope.UpperTeeth[i].m_ProbingDepth.b;
+            sum += $scope.UpperTeeth[i].m_ProbingDepth.c;
+            sum += $scope.UpperTeeth[i].m_ProbingDepthL.a;
+            sum += $scope.UpperTeeth[i].m_ProbingDepthL.b;
+            sum += $scope.UpperTeeth[i].m_ProbingDepthL.c;
+            sum += $scope.LowerTeeth[i].m_ProbingDepth.a;
+            sum += $scope.LowerTeeth[i].m_ProbingDepth.b;
+            sum += $scope.LowerTeeth[i].m_ProbingDepth.c;
+            sum += $scope.LowerTeeth[i].m_ProbingDepthL.a;
+            sum += $scope.LowerTeeth[i].m_ProbingDepthL.b;
+            sum += $scope.LowerTeeth[i].m_ProbingDepthL.c;
+        }
+        var average = Math.round(sum / 192 * 100) / 100;
+        return average;
+    };
+
+    $scope.SVGMissing = function(sum){
+        var Circumference = 2 * Radius * Math.PI;
+
+        var maxCount = 32;
+
+        var offset = -(Circumference / maxCount) * sum;
+        document.getElementById('crt_missing_c').setAttribute('stroke-dashoffset', offset);
     };
     $scope.sumMissingTeeth = function(){
         var sum = 0;
@@ -57,7 +129,17 @@ app.controller('ctl', function($scope){
             sum += !$scope.UpperTeeth[i].m_Exists;
             sum += !$scope.LowerTeeth[i].m_Exists;
         }
+        $scope.SVGMissing(sum);
         return Math.round(sum / 32 * 10000) / 100;
+    };
+
+    $scope.SVGImplants = function(sum){
+        var Circumference = 2 * Radius * Math.PI;
+
+        var maxCount = 32;
+
+        var offset = -(Circumference / maxCount) * sum;
+        document.getElementById('crt_implants_c').setAttribute('stroke-dashoffset', offset);
     };
     $scope.sumImplants = function(){
         var sum = 0;
@@ -66,6 +148,21 @@ app.controller('ctl', function($scope){
             sum += $scope.UpperTeeth[i].m_Implant;
             sum += $scope.LowerTeeth[i].m_Implant;
         }
+        $scope.SVGImplants(sum);
         return Math.round(sum / 32 * 10000) / 100;
     };
 });
+
+
+
+var Radius = 60;
+var Circumference = 2 * Radius * Math.PI;
+
+document.getElementById('crt_missing_c').setAttribute('stroke-dasharray', Circumference);
+document.getElementById('crt_missing_c').setAttribute('r', Radius);
+document.getElementById('crt_bleed_c').setAttribute('stroke-dasharray', Circumference);
+document.getElementById('crt_bleed_c').setAttribute('r', Radius);
+document.getElementById('crt_implants_c').setAttribute('stroke-dasharray', Circumference);
+document.getElementById('crt_implants_c').setAttribute('r', Radius);
+document.getElementById('crt_plaque_c').setAttribute('stroke-dasharray', Circumference);
+document.getElementById('crt_plaque_c').setAttribute('r', Radius);
